38fa7a6427e4941b9b3f2d2edeb8207c5becfe01
Fillipe Ogg <dupuqub@gmail.com>
2019-02-28 19:12:16 -0300

    Update full.js

--- bricks/full.js
+++ bricks/full.js
@@ -4,29 +4,48 @@
 //......................................................................................................................
 
 project.bricks.full = () =>
-({
-  id: `fullBack`,
-  classes: [`fadein`],
-  styles:
-  [
-    [`width`, `calc(var(--u) * 100)`],
-    [`height`, `calc(var(--u) * 100)`],
-    [`background-image`, `url(images/fullBack.png)`],
-    [`background-position`, `center`],
-    [`background-size`, `contain`],
-    [`cursor`, `pointer`],
-  ],
-  inner:
+{
+  const click = () =>
   {
-    id: `full`,
-    tag: `img`,
-    extras: [[`src`, `images/full.png`]],
+    const body = dunp.get(`body`)
+
+         if(body.requestFullscreen) body.requestFullscreen()
+    else if(body.msRequestFullscreen) body.msRequestFullscreen()
+    else if(body.mozRequestFullScreen) body.mozRequestFullScreen()
+    else if(body.webkitRequestFullscreen) body.webkitRequestFullscreen()
+  }
+
+  const brick =
+  {
+    id: `fullBack`,
+    classes: [`fadein`],
     styles:
     [
-      [`width`, `100%`],
-      [`height`, `100%`],
-      [`transition`, `all 0.2s`],
+      [`width`, `calc(var(--u) * 100)`],
+      [`height`, `calc(var(--u) * 100)`],
+      [`background-image`, `url(images/fullBack.png)`],
+      [`background-position`, `center`],
+      [`background-size`, `contain`],
+      [`cursor`, `pointer`],
     ],
-  },
-})
+    inner:
+    {
+      id: `full`,
+      tag: `img`,
+      extras:
+      [
+        [`src`, `images/full.png`],
+        [`onclick`, dunp.trigger(click)],
+      ],
+      styles:
+      [
+        [`width`, `100%`],
+        [`height`, `100%`],
+        [`transition`, `all 0.2s`],
+      ],
+    },
+  }
+
+  return brick
+}
 
