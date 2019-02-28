aad8d7140d9799b24afa8d3425639bdefb03214f
Fillipe Ogg <dupuqub@gmail.com>
2019-02-28 14:56:42 -0300

    Update portuguese.js

--- langs/portuguese.js
+++ langs/portuguese.js
@@ -8,7 +8,7 @@ project.langs.portuguese =
   id: `portuguese`,
   name: `Português`,
   title: `o quiz de ARTE`,
-  pageTitle: [`o${dunp.space(4)}QUIZ${dunp.space(8)}de`, `ARTE`],
+  pageTitle: [`o${dunp.space(4)}QUIZ${dunp.space(4)}de`, `ARTE`],
   play: `JOGAR`,
   question: `Quem pintou esta obra?`,
   confirm: `CONFIRMO`,
